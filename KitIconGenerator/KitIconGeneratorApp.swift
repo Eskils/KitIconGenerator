b590e1e24eb7d149ff17cce513a3d42ea85cbbdf
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2023-11-21 15:41:42 +0100

    Initial Commit

---
+++ KitIconGenerator/KitIconGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitIconGeneratorApp.swift
+//  KitIconGenerator
+//
+//  Created by Eskil Gjerde Sviggum on 21/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct KitIconGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
